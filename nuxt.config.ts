2da30d8e70eaed6cd59db9b162b04a64b5c4a44b
notKamui <teillard.jimmy@outlook.fr>
2023-04-01 19:00:00 +0200

    Nuxt plugin configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
+import { transformShortVmodel } from '@vue-macros/short-vmodel'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
@@ -6,9 +11,55 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
   },
-  imports: { dirs: ['stores'] },
+  imports: { dirs: ['stores'] }, // add folders here to auto-import them in your application
   components: [{ path: '~/components', pathPrefix: false }],
 
   // uncomment to disable SSR. This will basically make the app a SPA, like a normal Vue app, but with all the Nuxt goodies
   // ssr: false,
+
+  // global CSS files
+  css: [
+    '@unocss/reset/antfu.css',
+  ],
+
+  // plugin configurations
+  modules: [
+    '@vueuse/nuxt',
+    '@vue-macros/nuxt',
+    '@unocss/nuxt',
+    '@nuxtjs/critters',
+    '@nuxtjs/color-mode',
+    '@nuxt/image-edge',
+    '@pinia/nuxt',
+  ],
+  vue: {
+    compilerOptions: {
+      nodeTransforms: [transformShortVmodel({ prefix: '::' })],
+    },
+  },
+  vite: {
+    plugins: [
+      VueI18nVitePlugin({
+        include: [resolve(dirname(fileURLToPath(import.meta.url)), './locales/*.yml')],
+      }),
+    ],
+  },
+  macros: {
+    exportProps: true,
+    reactivityTransform: true,
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'color-scheme',
+  },
+  pinia: {
+    autoImports: [
+      'defineStore',
+      ['defineStore', 'definePiniaStore'],
+      'storeToRefs',
+    ],
+  },
 })
