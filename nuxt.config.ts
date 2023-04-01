db9c7bd0b91e4872882ce436469b2005dd14f5a2
notKamui <teillard.jimmy@outlook.fr>
2023-04-01 18:40:34 +0200

    Simple nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nightrunner',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  imports: { dirs: ['stores'] },
+  components: [{ path: '~/components', pathPrefix: false }],
 
+  // uncomment to disable SSR. This will basically make the app a SPA, like a normal Vue app, but with all the Nuxt goodies
+  // ssr: false,
 })
