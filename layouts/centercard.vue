407942a75923d797529a94e8c125245d2064272f
notKamui <teillard.jimmy@outlook.fr>
2023-04-27 16:41:37 +0200

    Updated dependencies

--- layouts/centercard.vue
+++ layouts/centercard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen">
-    <Header />
+    <NHeader />
     <div class="h-full flex-center flex-col gap-5">
       <div class="bg-white dark:bg-dark rounded-xl border-bluegray shadow-md border-1 border-opacity-25% p-10 w-100 sm:w-125 lg:w-170">
         <slot />
